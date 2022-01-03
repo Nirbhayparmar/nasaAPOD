6ef22d5d31275083f95c401711854f2c00b6058c
Nirbhay Parmar <niravnirbhay94@gmail.com>
2022-01-03 12:34:12 +0530

    created NASA APOD webapp

---
+++ apod.js
@@ -0,0 +1,34 @@
+let APIKEY = "u1RzRKQ1ka0MHSidfTReW8VDW1WXdJdwcdaFwMOB";
+let url =  "https://api.nasa.gov/planetary/apod?api_key=";
+let date = document.getElementById("date"); 
+let Heading = document.getElementById("title");
+let img = document.getElementById("image");
+let description = document.getElementById("detail");
+let copyright = document.getElementById("copyright");
+let btn = document.getElementById('btn');
+let btn1 = document.getElementById('btn1');
+function fetchData(){
+  let dateValue = date.value;
+  fetch(`${url}${APIKEY}&date=${dateValue}`)
+  .then(response => response.json())
+  .then(data => useData(data));
+}
+function fetchTodaysData(){
+  fetch(`${url}${APIKEY}`)
+  .then(response => response.json())
+  .then(data => useData(data));
+}
+
+function useData(data){
+    console.log(data);
+    let title = data.title;
+    let source = data.url;
+    Heading.innerHTML=title;
+    img.src=source;
+    description.innerHTML=data.explanation;
+    if(data.copyright){copyright.innerHTML=`Copyright: ${data.copyright}`;}
+    
+}
+// window.addEventListener('load', fetchData);
+btn.addEventListener('click',fetchData);
+btn1.addEventListener('click',fetchTodaysData);
